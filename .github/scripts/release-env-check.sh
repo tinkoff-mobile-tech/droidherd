adf9097dbe1c652e0cbfd8113f91f6a46bc475c0
Roman Torsten <r.torsten@tinkoff.ru>
2023-03-31 12:34:31 +0300

    Build and push dockerfile with github actions.

--- .github/scripts/release-env-check.sh
+++ .github/scripts/release-env-check.sh
@@ -3,10 +3,10 @@
 echo "Checking repository [$GITHUB_REPOSITORY]..."
 [[ "${GITHUB_REPOSITORY}" != "tinkoff-mobile-tech/droidherd" ]] && exit 1
 
-#echo "Checking that ref type is tag ..."
-#[[ "${GITHUB_REF_TYPE}" != "tag" ]] && exit 1
-#
-#echo "Checking tag [$GITHUB_REF_NAME]..."
-#[[ "${GITHUB_REF_NAME}" =~ ^release-[0-9]+\.[0-9]+\.[0-9]+$ ]] || exit 1
+echo "Checking that ref type is tag ..."
+[[ "${GITHUB_REF_TYPE}" != "tag" ]] && exit 1
+
+echo "Checking tag [$GITHUB_REF_NAME]..."
+[[ "${GITHUB_REF_NAME}" =~ ^release-[0-9]+\.[0-9]+\.[0-9]+$ ]] || exit 1
 
 echo "Ok, lets go"
