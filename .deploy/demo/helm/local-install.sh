c4b8c7f84d5109496ad65f9dadbd3f82484adef3
Roman Torsten <r.torsten@tinkoff.ru>
2023-03-31 12:52:23 +0300

    Build and push dockerfile with github actions. Add release pipeline.

--- .deploy/demo/helm/local-install.sh
+++ .deploy/demo/helm/local-install.sh
@@ -3,7 +3,7 @@
 # this is helper script to quick install helm template
 
 # register droidherd CRD in k8s cluster
-# kubectl apply -f ../../../crd-model/src/main/crd/testops.tinkoff.ru_droidherdsessions.yaml
+kubectl apply -f ../../../crd-model/src/main/crd/testops.tinkoff.ru_droidherdsessions.yaml
 
 # deploy service
 helm -n droidherd-service upgrade --install droidherd-service .
